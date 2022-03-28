7027b70c6091db3b25b9d1dbcc63934012c9cfa8
Trafael Lewis <TrafLewis@gmail.com>
2022-03-27 20:05:28 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName(customerName) {
+    return customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+var bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = "jim"
+}
+
+const leastFavoriteCustomer = "rupert"
+
+function changeLeastFavoriteCustomer() {
+    var leastFavoriteCustomer = "dillan"
+}
